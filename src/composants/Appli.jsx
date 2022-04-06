8ca2368b34e87af81cca37d4a330f5eaf0ec54a2
Hervee Charbonneau <e1255799@cmaisonneuve.qc.ca>
2022-04-06 19:11:06 -0400

    Test commit

--- src/composants/Appli.jsx
+++ src/composants/Appli.jsx
@@ -9,16 +9,16 @@ export default function Appli() {
 
   return (
     // 1)  Si un utilisateur est connecté : 
-      // <div className="Appli">
-      //   <header className="appli-entete">
-      //     <img src={logo} className="appli-logo" alt="Memo" />
-      //     <Utilisateur />
-      //   </header>
-      //   <Taches />
-      //   <Controle />
-      // </div>
+      <div className="Appli">
+        <header className="appli-entete">
+          <img src={logo} className="appli-logo" alt="Memo" />
+          <Utilisateur />
+        </header>
+        <Taches />
+        <Controle />
+      </div>
 
     // 2) Par contre si aucun utilisateur n'est connecté, on affiche plutôt le composant suivant : 
-      <Accueil />
+      // <Accueil />
   );
 }
